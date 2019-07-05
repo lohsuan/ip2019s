cc0b1a5e390bf0cee4ec81acd113548e89e60470
lohsuan <52515354+lohsuan@users.noreply.github.com>
2019-07-05 17:32:11 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -1,9 +1,43 @@
 /*
-
+    https://www.cns11643.gov.tw/wordView.jsp?ID=0
 */
 
 
 function start(e) {
+    var name = [
+        {
+            char:'羅',
+            big5:'C3B9',
+            cns:'1-7865',
+            unicode:'7F85'
+        },
+        {
+            char:'羽',
+            big5:'A6D0',
+            cns:'1-4851',
+            unicode:'7FBD'
+        },
+        {
+            char:'軒',
+            big5:'B061',
+            cns:'1-5846',
+            unicode:'8ED2 '
+        }
+    ];
+
+    var ele = document.getElementById("div1");
+
+    for(var i=0; i<3; i++){
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += 'big5 = ';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += 'cns = ';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += 'unicode = ';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '<br>';
+    }
+
 
 }
 
